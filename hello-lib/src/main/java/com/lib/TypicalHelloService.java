a7db94314a5f52f543ef9da330d2de445ce0ef6e
sashinpivotal <sashin@pivotal.io>
2019-03-05 22:12:01 -0500

    more wording change

--- hello-lib/src/main/java/com/lib/TypicalHelloService.java
+++ hello-lib/src/main/java/com/lib/TypicalHelloService.java
@@ -1,11 +1,13 @@
 package com.lib;
 
-// TODO-12: Review "hello-lib" project - it is just
-//          a library that contains "TypicalHelloService"
-//          as shown below.
+// TODO-12: Study "hello-lib" project
+//          It is just a library that contains this "TypicalHelloService"
+//          class.
 public class TypicalHelloService implements HelloService {
+
     @Override
     public void greet() {
         System.out.println("Hello, Typical!");
     }
+
 }
